c1af9c1db0d0dd0f33d4a158c80cddb017aefe51
muhammad mavia <muhammadmavia172@gmail.com>
2016-03-21 13:25:52 +0500

    ur change in otherwise

--- public/js/main.js
+++ public/js/main.js
@@ -160,7 +160,7 @@ MetronicApp.controller('FooterController', ['$scope', function($scope) {
 MetronicApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
     // Redirect any unmatched url
-    $urlRouterProvider.otherwise("/app/dashboard");
+    $urlRouterProvider.otherwise("/login");
 
     $stateProvider
 
